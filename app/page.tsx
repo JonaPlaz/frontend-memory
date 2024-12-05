64342664b705df0af78d927c8fc042ab3af75c52
JonaPlaz <jplazanet@hotmail.com>
2024-12-05 13:34:29 +0100

    feat: upgrade homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 import { useChessFactory } from "@/hooks/useContract";
 
+import Connect from "@/components/shared/Connect/Connect";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import GameList from "@/components/shared/GameList/GameList";
+
 export default function Home() {
   const chessFactory = useChessFactory;
 
+  //  prochaine étape, on vire le create, on fait 3 create game en local sur le back
+  // on crée un composant GameList et ses sous composants (qui récupèrera) les games et on affiche les games
   const createGame = async () => {
     console.log(chessFactory);
   };
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Chess - Blitz</h1>
-      <button onClick={createGame} className="btn btn-primary">
-        Create Game
-      </button>
-    </main>
+    <>
+      <div className="my-4">
+        <Connect />
+      </div>
+      <ConnectButton />
+      <div className="my-8">
+        <GameList />
+      </div>
+    </>
   );
 }
---
+++ components/shared/Connect/Connect.tsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+
+export class Connect extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2 className="font-bold text-lg">Connectez votre wallet</h2>
+        <h3 className="text-lg">Pour commencer à jouer</h3>
+      </div>
+    );
+  }
+}
+
+export default Connect;
